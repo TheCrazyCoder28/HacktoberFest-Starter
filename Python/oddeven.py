f732ccfda05f00b15f72fc9d6fe12f83563b009f
Kamal Kumar Jena <72182167+Kamal-Kumar-Jena@users.noreply.github.com>
2021-10-02 21:16:55 +0530

    Update oddeven.py

--- Python/oddeven.py
+++ Python/oddeven.py
@@ -2,8 +2,25 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        if (num % 2) == 0:
+            print(f"{num} is Even")
+            print("=============================")
+            a=input("Enter 'y' for using the program again: ")
+            if a.lower() == "y":
+                continue
+            else:
+                break
+        else:
+            print(f"{num} is Odd")
+            print("=============================")
+            a=input("Enter 'y' for using the program again: ")
+            if a.lower() == "y":
+                continue
+            else:
+                break
+    except ValueError:
+        print("Enter a valid number")
+print("Thanks for using our program :)")
